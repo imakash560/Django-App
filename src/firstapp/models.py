0b6afd6e82fb033fa90ac47b6e0ff4166ffd0cae
anushiv <shuklanushiv@gmail.com>
2018-11-07 15:33:14 +0530

    changed models as per Polls app

--- src/firstapp/models.py
+++ src/firstapp/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 #MVC
 
-class Post(models.Model):
-	title = models.CharField(max_length=120)
-	content = models.TextField()
-	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
-	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+class Question(models.Model):
+	question_text=models.CharField(max_length=200)
+	pub_date=models.DateTimeField('date published')
 
-	def __str__(self):
-		return self.title
+class Choice(models.Model):
+	question=models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text=models.CharField(max_length=200)
+	votes=models.IntegerField(default=0)
