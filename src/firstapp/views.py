bc4a2d99ad5a418ca4518cd3618e0f0fd5c0dd67
anushiv <shuklanushiv@gmail.com>
2018-11-13 11:27:42 +0530

    Updated Choices

--- src/firstapp/views.py
+++ src/firstapp/views.py
@@ -15,10 +15,12 @@ def index(request):
 	return render(request,'firstapp/index.html',context)
 
 def detail(request,question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+	question=Question.objects.get(pk=question_id)
+	return render(request,'firstapp/detail.html',{'question':question})
 def results(request,question_id):
 	response="You're looking at the results of question %s."
 	return HttpResponse(response % question_id)
 def vote(request,question_id):
 	return HttpResponse("You're voting on question %s." % question_id)
+
 
