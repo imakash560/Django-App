aeeed13360d944a2ce531995e9f2ae33625468c1
anushiv <shuklanushiv@gmail.com>
2018-11-15 18:05:03 +0530

    commit

--- src/firstapp/views.py
+++ src/firstapp/views.py
@@ -30,9 +30,11 @@ def addq(request):
 def detail(request,question_id):
 	question=get_object_or_404(Question,pk=question_id)
 	return render(request,'firstapp/detail.html',{'question':question})
+
 def results(request,question_id):
 	response="You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+	return render(request,'firstapp/results.html')
+
 def vote(request,question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	try:
